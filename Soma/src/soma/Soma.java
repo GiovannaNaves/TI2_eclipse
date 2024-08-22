2006d2e66f4c3e1a891a10916772039204aba6a1
GiovannaNaves <110492330+GiovannaNaves@users.noreply.github.com>
2024-08-22 20:26:19 -0300

    Soma

---
+++ Soma/src/soma/Soma.java
@@ -0,0 +1,22 @@
+package soma;
+
+import java.util.*;
+
+class Soma {
+
+    public static Scanner teclado = new Scanner(System.in);
+
+    public static void main (String args[]){
+        //Declaracao de variaveis
+        int num1, num2, soma;
+        //Leituras
+        System.out.println("Digite um número");
+        num1 = teclado.nextInt();
+        System.out.println("Digite outro número");
+        num2 = teclado.nextInt();
+        //Somar
+        soma = num1 + num2;
+        //Mostrar na tela
+        System.out.println("Soma:" + soma);
+        }
+}
